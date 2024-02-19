2fbdf29f137ee61d070de6fbd5dddc04d7e73358
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-02-19 14:11:22 +0200

    new List

---
+++ ListCreate/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ListCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete spisuk ot colors> //Done// za end!");
+            List<string> colorList = new List<string>();
+            string input = Console.ReadLine();
+            while(input != "Done")
+            {
+                //mush in colorList
+                colorList.Add(input);
+                input = Console.ReadLine();
+            }
+            for (int i = 0; i < colorList.Count; i++)
+            {
+                Console.WriteLine(colorList[i]);
+            }
+
+
+             Console.WriteLine("Koi element da iztriesh 0-" + (colorList.Count - 1 + " ? "));
+            int index = int.Parse(Console.ReadLine());
+
+            colorList.RemoveAt(index);
+
+            colorList[0] = "Rozovo";
+
+            foreach (string chervenoKvadratche in colorList)
+            {
+                Console.WriteLine(chervenoKvadratche);
+            }
+
+
+
+
+        }
+    }
+}
